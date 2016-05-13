27a1a328ce9b4b36b223ae579dc4e324102cd1f5
Dason <dwood15@gmail.com>
2016-05-13 15:48:22 -0600

    First commit - Added timing constraints so we don't need to worry about the number of requests.

---
+++ FriendFinderMain.py
@@ -0,0 +1,25 @@
+from riotwatcher import RiotWatcher
+
+w = RiotWatcher('f9f71a4c-a2fd-4d5c-a3b9-92a7a573771a')
+
+# check if we have API calls remaining
+print(w.can_make_request())
+
+me = w.get_summoner(name='imaqtpie')
+print(me)
+print(me['id'])
+# takes list of summoner ids as argument, supports up to 40 at a time
+# (limit enforced on riot's side, no warning from code)
+#my_mastery_pages = w.get_mastery_pages([me['id'], ])[str(me['id'])]
+# returns a dictionary, mapping from summoner_id to mastery pages
+# unfortunately, this dictionary can only have strings as keys,
+# so must convert id from a long to a string
+
+#print(my_mastery_pages)
+
+#my_ranked_stats = w.get_ranked_stats(me['id'])
+#print(my_ranked_stats)
+
+my_ranked_stats_last_season = w.get_ranked_stats(me['id'], season=2016)
+print(my_ranked_stats_last_season)
+
