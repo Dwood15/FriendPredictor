f7cf0d82b9a80edd7c4d3862770b624ebaaf4fad
Dason Woodhouse <dwood15@gmail.com>
2016-06-18 02:03:41 -0600

    Storing tweets.

--- TwitterStatusFinder.py
+++ TwitterStatusFinder.py
@@ -12,28 +12,29 @@ from retrying import retry
 class TwitterStatusFinder:
 	def __init__(self, selection_query, test = False):
 		self.api = tools.api_connect()
-		tup = tools.db_connect()
+		tmp = tools.db_connect()
 		#see the tuples in tools.py.
-		self.dbcur = tup.cursor()
-		
-		print "Executing query!"
-		
+		self.selcur = tmp.cursor()
+		self.inscur = tmp.cursor()
+
 		try:
 		#pull the selected accounts from the database.
 			if(test):
 				print "Testing!"
-				self.dbcur.execute(selection_query + " LIMIT 1")
+				self.selcur.execute(selection_query + " LIMIT 1")
 				print "Fetching one!"
-				self.currentUser = self.dbcur.fetchone()
+				self.currentUser = self.selcur.fetchone()
 				
 				if(self.currentUser is not None):
 					print "Success! got a row: " + str(self.currentUser)
-				self.get_tweets()
-				self.dbcur.close()
+				
+				self.get_tweets(self.currentUser, count=200)
+				self.store_tweet()
+				self.selcur.close()
 			else:
-				self.dbcur.execute(selection_query + "ORDER BY tweet_count, DESC")
+				self.selcur.execute(selection_query + "ORDER BY tweet_count, DESC")
 				print 'Getting Many users'
-				self.currentUser = dbcur.fetchmany(100)
+
 				
 			if self.currentUser is not None:
 				print "Success! Connected to database, and recvd data!"
@@ -51,33 +52,91 @@ class TwitterStatusFinder:
 		#don't want to close the cursor when we don't have to...
 		finally:
 			pass 
+	
+	def store_tweet(self, tweet, cursor, userData, minMax, test=False):
+		print "Storing tweet by id: " + str(tweet_id)
+		tweet_id = tweet.id
+		text = tweet.text.encode('ascii', 'ignore')
+		print "Encoded text: " + text
+		date = str(tweet.created_at).encode('ascii', ignore)
+		print "Encoded Date: " + date
+		user_id =  int(tweet.id)
+		print "Encoded user_id: " + str(user_id)
+		rt_count = tweet.retweet_count
+		print "Retweet count: " + str(rt_count)
+		reply_to_tweet = int(tweet.in_reply_to_status_id)
+		print "Reply to tweet " + str(reply_to_tweet)
 		
-	def get_tweets(self):
-		if(self.currentUser is not None):
+		reply_to_user = int(tweet.in_reply_to_twitter_id)
+		print "Reply to user " + str(reply_to_user)
+		
+		
+		insert_script = "INSERT INTO tweets " \
+						"(tweet_id, tweet_text, created_at," \
+						" twitter_id, retweet_count," \
+						" in_reply_to_status_id, in_reply_to_twitter_id," \
+						" possibly_sensitive)" \
+						" VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+		args = (tweet_id, text, date, user_id, rt_count, reply_to_tweet, reply_to_user, int(twitter_id))
+		print "\nAll args: " + args
+		#try: 
+			#cursor.execute(insert_script, args)
+	#get users in groups of 100 by default.
+	def iter_users(self, num=100):
+		while True:
+			users = self.selcur.fetchmany(num)
+			if not users:
+				break
+			for user in users:
+				yield user
+			
+	def get_tweets(self, user_Id=None, count=200, dbcur):
+		if(user_Id is not None):
+			userId = user_Id[0]
+			min_id = user_Id[1]
+			max_id = user_Id[2]
 			currentTweet = None
-			if(self.currentUser[1] is not None and self.currentUser[2] is not None):
+			tweets = []
+			if(min_id is not None and max_id is not None):
+				currentTweet = next(tweepy.Cursor(self.api.user_timeline, trim_user=True, user_id=userId, count=count, contributor_details=False, since_id=max_id))
 				print "We've stored this user's tweets before!"
 			
-			elif(self.currentUser[1] is None and self.currentUser[2] is None):
-				print "storing for new user!"
-				currentTweet = next(tweepy.Cursor(self.api.user_timeline, user_id=self.currentUser[0], count=1, trim_user=True, contributor_details=False).items(1))
+			elif(min_id is None and max_id is None):
+				print "Storing for new user!"
+				currentTweet = tweepy.Cursor(self.api.user_timeline, trim_user=True, user_id=userId, count=count, contributor_details=False).items()
 				
 			else:
-				print "WHOA, half-baked ids up in here! careful bessy."
-				print "Don't worry, we'll just overwrite them I guess."
+				print "WHOA, half-baked ids up in here! Slow down bessy."
+				print "user_id: " + str(userId) + " min_id: " + str(min_id) + " max_id: " + str(max_id)
+				print "Exiting..."
+				sys.exit(1)
 			
 			if(currentTweet is not None):
-				print "Received tweet! Here's the text: "
-				print currentTweet.text
+				print "Received tweets! Here's the text: "
+				upUser = False
+				for tweet in currentTweet:
+					print tweet.text
+					if(tweet.id > max_id or max_id is None):
+						max_id = tweet.id
+					elif (tweet.id < min_id is None):
+						min_id = tweet.id
+						
 			else:
-				print "Didn't get anything from the tweet query."
-	def process_statuses(self):
-		pass
-	def get_next_user(self):
-		print 'Getting next user!'
-		self.currentUser = next(self.currentUser)
-		
-		
+				print "Didn't get anything from tweets query!"
+		else:
+			print "User_id is none, please specify a valid user_id"
+			sys.exit(1)
+			
+		for tweet in tweets:
+			store_tweet(tweet, self.dbcur, )
+		#essentially: 
+		#for account in accounts:
+		#	tweets_add = []
+		#	for tweet in tweets:
+		#		tweets_add.add(tweet)
+			#build list of tweets in sql-handy format
+			#insert into database
+			
 #'gaiqtren' = 'get_any_id_query_tweet_count_range_english'
-gaiqtcr = "SELECT twitter_id, smallest_pulled_tweet_id, highest_pulled_tweet_id FROM twitter_entity WHERE tweet_count > 0 AND tweet_count < 50 "
-tsf = TwitterStatusFinder(gaiqtcr, True)
+select_script = "SELECT twitter_id, smallest_pulled_tweet_id, highest_pulled_tweet_id FROM twitter_entity WHERE tweet_count > 0 AND tweet_count < 10 "
+tsf = TwitterStatusFinder(select_script, True)
